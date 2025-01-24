e882cf9fdc987bd271fc949744d05bc50778a4cc
ayoubmorchid <ayoubmorchid.jobs@gmail.com>
2025-01-24 17:23:08 +0000

    add privateroute link to app.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,32 @@
-// App.js
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-import SignUpForm from './pages/register';
+import SignUpForm from "./pages/register";
 import Login from "./pages/login";
 import Books from "./pages/Books";
-import ForgetPassword from './pages/ForgetPassword';
-import ContactUs from './pages/contactus';
-import AboutUs from './pages/about';
-import NotFound from "./pages/NotFound"; 
+import ForgetPassword from "./pages/ForgetPassword";
+import ContactUs from "./pages/contactus";
+import AboutUs from "./pages/about";
+import NotFound from "./pages/NotFound";
 import Checkout from "./pages/Checkout";
-import './style/global.css';
-
-
-
-
-
-
-
+import PrivateRoute from "./components/PrivateRoute"; // استيراد PrivateRoute
+import "./style/global.css";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUpForm/>} />
+        <Route path="/signup" element={<SignUpForm />} />
         <Route path="/login" element={<Login />} />
         <Route path="/books" element={<Books />} />
         <Route path="/forget-password" element={<ForgetPassword />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/about" element={<AboutUs />} />
-        <Route path="*" element={<NotFound />} /> 
-        <Route path="checkout" element={<Checkout/>} />
-
-
-
+        <Route path="*" element={<NotFound />} />
+        <Route path="/checkout" element={<PrivateRoute element={Checkout} />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
