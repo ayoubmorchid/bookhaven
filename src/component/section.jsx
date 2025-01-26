447fced5b985f15b6ae6579a58bc63fb551e9383
unknown <ayoubmorchid.jobs@gmail.com>
2025-01-26 11:11:29 +0000

    change welcome section in landing page

--- src/component/section.jsx
+++ src/component/section.jsx
@@ -4,22 +4,27 @@ import book1 from "../images/book1.png";
 import book2 from "../images/book2.png";
 import book3 from "../images/book3.png";
 import book4 from "../images/book4.png";
-import book5 from '../images/book5.png';
+import book5 from "../images/book5.png";
 import photo1 from '../images/photo1.jpg';
 import '../style/section.css';
 
 export default function Section() {
   return (
     <div className="section">
-      <div className="image">
-        <img src={girl} alt="Girl illustration" />
+      {/* Welcome Section */}
+      <div className="welcome-container">
+        <div className="welcome-text">
+          <h1>Welcome To EpicReads</h1>
+          <p>We're so glad you're here</p>
+          <h5>73,530 books with audio in different languages. Read for free, listen for $4/month</h5>
+          <button className="welcome-button">Find Your Book</button>
+        </div>
+        <div className="welcome-image">
+          <img src={girl} alt="Girl illustration" />
+        </div>
       </div>
-      <div className="welcom">
-        <h1>Welcome To EpicReads</h1>
-        <p>We're so glad you're here</p>
-        <h5>73,530 books with audio in different languages. Read for free, listen for $4/month</h5>
-        <input value="Find Your Book" />
-      </div>
+
+      {/* Keep Popular Books As Is */}
       <div className="description">
         <h1>
           Find your next great <br />
@@ -51,16 +56,6 @@ export default function Section() {
         </div>
         <button>See All →</button>
       </div>
-      <div className="desc">
-        <img src={photo1} alt="Library" />
-        <h2>
-          Step into a world of stories <br /> and imagination
-        </h2>
-        <p>
-          Dive into our curated collection of bestsellers, hidden gems, and timeless classics, all at unbeatable prices. Plus, enjoy exclusive discounts and special offers on thousands of titles. Your next literary adventure starts here.
-        </p>
-      </div>
-      <div className="foter"></div>
     </div>
   );
 }
