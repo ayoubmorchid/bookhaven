f06d5840961845edfffc0797402deb27fe9f2151
unknown <ayoubmorchid.jobs@gmail.com>
2025-01-29 21:32:25 +0000

    add style to page payment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ContactUs from "./pages/contactus";
 import AboutUs from "./pages/about";
 import NotFound from "./pages/NotFound";
 import Checkout from "./pages/Checkout";
-import PrivateRoute from "./component/PrivateRoute"; // استيراد PrivateRoute
+import PrivateRoute from "./component/PrivateRoute"; 
 import "./style/global.css";
 import Payment from "./pages/Payment";
 
