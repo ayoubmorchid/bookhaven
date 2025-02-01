c5081327a92bf8d60c3118dde5d16ff59ce1e2a8
unknown <ayoubmorchid.jobs@gmail.com>
2025-02-01 22:41:33 +0000

    fix the code

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -5,6 +5,9 @@ import { useNavigate } from "react-router-dom";
 
 const Checkout = () => {
   const { cartItems, updateQuantity, removeFromCart } = useContext(CartContext);
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const navigate = useNavigate(); 
+
 
 
   const calculateTotal = () => {
