64bd473cad0be8abc2507b15c6083788fb26611b
Ayoub Morchid <ayoubmorchid.jobs@gmail.com>
2026-01-01 15:01:17 +0100

    Configure React router structure

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/home";
-import SignUpForm from "./pages/register";
-import Login from "./pages/login";
 import Books from "./pages/Books";
-import ForgetPassword from "./pages/ForgetPassword";
-import ContactUs from "./pages/contactus";
 import AboutUs from "./pages/about";
-import NotFound from "./pages/NotFound";
+import ContactUs from "./pages/contactus";
+import Login from "./pages/login";
+import Register from "./pages/register";
 import Checkout from "./pages/Checkout";
-import PrivateRoute from "./component/PrivateRoute"; 
-import "./style/global.css";
 import Payment from "./pages/Payment";
-
-
+import ForgetPassword from "./pages/ForgetPassword";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUpForm />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/books" element={<Books />} />
-        <Route path="/forget-password" element={<ForgetPassword />} />
-        <Route path="/contact" element={<ContactUs />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/checkout" element={<PrivateRoute element={Checkout} />} />
-        <Route path="/payment" element={<Payment />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route path="/books" element={<Books />} />
+      <Route path="/about" element={<AboutUs />} />
+      <Route path="/contact" element={<ContactUs />} />
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Register />} />
+      <Route path="/forget-password" element={<ForgetPassword />} />
+
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/payment" element={<Payment />} />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
