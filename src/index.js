266398b91520ed27aab1cf7b98fcd8a727cda831
Ayoub Morchid <ayoubmorchid.jobs@gmail.com>
2026-01-02 15:08:05 +0100

    Wrap app with CartProvider and BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { CartProvider } from './context/CartContext'; // استيراد CartProvider
+import { CartProvider } from './context/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <CartProvider> {/* تغليف التطبيق بـ CartProvider */}
-      <App />
-    </CartProvider>
+    <BrowserRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
