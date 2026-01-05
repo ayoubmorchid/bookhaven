3ed8cf11c86426638ce083a35965b36e31c511eb
Ayoub Morchid <ayoubmorchid.jobs@gmail.com>
2026-01-05 15:13:14 +0100

    Configure Axios base API

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,11 @@
-// src/config/api.js
 import axios from "axios";
 
-export default axios.create({
+const api = axios.create({
   baseURL: "http://127.0.0.1:8000/api",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
 });
+
+export default api;
